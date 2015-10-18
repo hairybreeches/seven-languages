c1ca45bd040bac72c539074188981f91d7b9de89
hairybreeches <hairybreeches@localhost>
2015-10-18 12:23:32 +0100

    Dijkstra's barber problem

--- src/seven_languages_clojure/core.clj
+++ src/seven_languages_clojure/core.clj
@@ -1,5 +1,57 @@
 (ns seven-languages-clojure.core)
 
+(def queue (agent 0))
+(def barber (agent 0))
+
+;code for shaving people in the queue
+(defn finish-shaving
+  [length]
+  (dec length))
+
+(defn shave-customer
+  [already-shaved]
+  (Thread/sleep 20)
+  (send queue finish-shaving)
+  (inc already-shaved))
+
+;code for updating queue with new arrivals
+(defn get-queue-interval
+  []
+  (+ 10 (rand 20)))
+
+(defn wait-for-next-arrival
+  []
+  (Thread/sleep (get-queue-interval)))
+
+(defn add-new-arrival
+  [length]
+  (if (= length 4)
+      length
+      (do
+        (send barber shave-customer)
+        (inc length))))
+
+(defn run-queue
+  []
+  (while true
+    (do
+      (wait-for-next-arrival)
+      (send queue add-new-arrival))))
+
+(defn calculate-shaves
+  []
+  (.start (Thread. run-queue))
+  (Thread/sleep 10000)
+  @barber)
+
+(defn -main
+  [& args]
+  (prn "Calculating shaves given in 10 seconds")
+  (prn "Maximum number of shaves possible is 499")
+  (prn "Minimum number of shaves possible is 332")
+  (prn "Shaves given: " (calculate-shaves))
+  (prn "Customers in queue at end: " @queue))
+
 
 
 
