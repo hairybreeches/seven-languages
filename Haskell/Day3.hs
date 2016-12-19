d3fc4555d8e8dd10e7be6659374c3ff088a17c13
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-12-19 17:20:30 +0000

    a gnarly map

---
+++ Haskell/Day3.hs
@@ -0,0 +1,21 @@
+module Day3 where
+
+import qualified Data.Map.Strict as Map
+
+type BadAssLookup = Map.Map Int (Map.Map Int (Map.Map Int (Map.Map Int String)))
+
+exampleLookup :: BadAssLookup
+exampleLookup = Map.fromList
+    [ 
+        (0, Map.fromList [
+            (1, Map.singleton 0 (Map.singleton 1 "New year")), 
+            (2, Map.singleton 0 (Map.singleton 5 "May day (replacement)"))]),
+        
+        (2, Map.fromList [
+            (5, Map.singleton 0 (Map.singleton 3 "Good Friday")), 
+            (6, Map.singleton 1 (Map.singleton 2 "Boxing day")),
+            (7, Map.singleton 1 (Map.singleton 2 "Christmas day (replacement)")),
+            (8, Map.singleton 0 (Map.singleton 3 "Easter Monday"))]),
+        
+        (3, Map.singleton 0 (Map.singleton 0 (Map.singleton 5 "Spring")))]
+
